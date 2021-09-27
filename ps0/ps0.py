aa13c31621888285a2a336f87e857946a7037f7d
MSDhanalekshmi <dhanalekshmi.12001362@lpu.in>
2021-09-27 13:00:16 +0530

    solution-to-ps0

---
+++ ps0/ps0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 27 sept 2021
+@author: MS Dhanalekshmi
+"""
+
+import numpy
+
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+
+print('x**y = {}'.format(x**y))
+print('log(x) = {}'.format(int(numpy.log2(x))))
